783a5844d3d703a9a6be49f6c48afdc6409703f8
Shahzaib Manzoor <34761350+shahzi113awan@users.noreply.github.com>
2023-01-10 01:12:13 -0800

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 const hbs = require('hbs')
 const app = express()
-const port =process.env.PORT || 3000
+const port =process.env.PORT || 5000
 //console.log(app.use(express.static(path.join(__dirname ,'../views'))) )
 
 const publicPath=path.join(__dirname ,'../public')
